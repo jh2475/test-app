e373c5d5d3f2d8ba973ff2ca9c86c3135043fcdb
leejuhyeong <dlwngud403@naver.com>
2024-04-24 11:03:19 +0900

    <MeasureEx />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Clock from './Clock';
 import MeasureEx from './MeasureEx';
 
 function App() {
   return (
     <>
-      <MeasureEx />
+      <Clock />
     </>
 
   );
